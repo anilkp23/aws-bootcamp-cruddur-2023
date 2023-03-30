35f25ae4b66d65245631d11bb845975c91c44614
anilkp23 <akpanikar@gmail.com>
2023-03-30 18:34:45 +0000

    Comment X-RAY

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -23,9 +23,9 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
-# X-RAY -----------------
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+# # X-RAY -----------------
+# from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # Honeycomb ------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -33,9 +33,9 @@ provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
-# X-RAY -----------------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+# # X-RAY -----------------
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 # Show this in the logs in backend-flask (STDOUT)
@@ -47,8 +47,8 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
-#XRAY ----------------
-XRayMiddleware(app, xray_recorder)
+# #XRAY ----------------
+# XRayMiddleware(app, xray_recorder)
 
 # Honeycomb ------
 # Initialize automatic instrumentation with Flask
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,15 +3,15 @@ from opentelemetry import trace
 
 from lib.db import pool, query_wrap_array
 
-tracer = trace.get_tracer("home.activities")
+# tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
-    #logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activites-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+    # #logger.info("HomeActivities")
+    # with tracer.start_as_current_span("home-activites-mock-data"):
+    #   span = trace.get_current_span()
+    #   now = datetime.now(timezone.utc).astimezone()
+    #   span.set_attribute("app.now", now.isoformat())
       
       sql = query_wrap_array("""
       SELECT
